b9b62527930d0f8f281ebb433435ea8de837550e
king <haruk@DESKTOP-U7EIVLC>
2020-06-29 15:24:04 +0900

    モーダル修正

--- src/main/frontend/src/components/Top.js
+++ src/main/frontend/src/components/Top.js
@@ -1,35 +1,19 @@
 import React from "react";
 import "../style/Top.css";
-import {
-  Button,
-  ButtonToolbar,
-  Modal,
-  ModalBody,
-  ModalDialog,
-  Form,
-  FormControl,
-  FormLabel,
-  FormText,
-  FormGroup,
-  ModalFooter,
-  ModalTitle,
-} from "react-bootstrap";
-import ModalHeader from "react-bootstrap/ModalHeader";
-
+import { Button, ButtonToolbar, Modal, Form } from "react-bootstrap";
 class Top extends React.Component {
-	constructor(props, context) {
+  constructor(props, context) {
     super(props, context);
 
-		this.state = {
-			loginShow: false,
-			registerShow: false,
-		};
-    
-	}
+    this.state = {
+      loginShow: false,
+      registerShow: false,
+    };
+  }
 
   render() {
-		let loginClose = () => this.setState({ loginShow: false });
-		let registerClose = () => this.setState({ registerShow: false });
+    let loginClose = () => this.setState({ loginShow: false });
+    let registerClose = () => this.setState({ registerShow: false });
 
     return (
       <div>
@@ -39,27 +23,23 @@ class Top extends React.Component {
             <Button onClick={() => this.setState({ loginShow: true })}>
               ログイン
             </Button>
-            <Button onClick={() => this.setState({ registerShow: true })} style={{ marginLeft: 5 }}>
+            <Button
+              onClick={() => this.setState({ registerShow: true })}
+              style={{ marginLeft: 5 }}
+            >
               新規会員登録
             </Button>
 
-            <Modal
-              show={this.state.loginShow}
-              onHide={loginClose}
-            >
+            <Modal show={this.state.loginShow} onHide={loginClose}>
               <Modal.Header closeButton>
-                <Modal.Title>
-                  ログイン
-                </Modal.Title>
+                <Modal.Title>ログイン</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <Form>
-                  <Form.Group controlId="formBasicEmail"  className="m-3">
+                  <Form.Group controlId="formBasicEmail" className="m-3">
                     <Form.Label className="mb-3">メールアドレス</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                      We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Text className="text-muted"></Form.Text>
                   </Form.Group>
 
                   <Form.Group controlId="formBasicPassword" className="m-3">
@@ -81,47 +61,43 @@ class Top extends React.Component {
               </Modal.Footer>
             </Modal>
 
-            <Modal
-              show={this.state.registerShow}
-              onHide={registerClose}
-            >
+            <Modal show={this.state.registerShow} onHide={registerClose}>
               <Modal.Header closeButton>
-                <Modal.Title>
-                  アカウントを作成
-                </Modal.Title>
+                <Modal.Title>アカウントを作成</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <Form>
                   <Form.Group controlId="formBasicName" className="m-3">
                     <Form.Label className="mb-3">ユーザーネーム</Form.Label>
-                    <Form.Control type="name" placeholder="User name" />
+                    <Form.Control type="name" placeholder="Username" />
                   </Form.Group>
 
                   <Form.Group controlId="formBasicEmail" className="m-3">
                     <Form.Label className="mb-3">メールアドレス</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                      We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Text className="text-muted"></Form.Text>
                   </Form.Group>
 
                   <Form.Group controlId="formBasicPassword" className="m-3">
-                    <Form.Label className="mb-3">パスワード</Form.Label>
+                    <Form.Label className="mb-3">
+                      パスワード
+                      <span id="characterlimit">(半角英数字8~16桁)</span>
+                    </Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                   </Form.Group>
                   <Form.Group controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                   </Form.Group>
-                  <Button variant="primary" type="submit"  className="m-3">
+                  <Button variant="primary" type="submit" className="m-3">
                     登録
                   </Button>
                 </Form>
               </Modal.Body>
-                <Modal.Footer>
-                  <Form.Text className="text-muted m-3">
-                    すでにアカウントをお持ちの場合、ログイン
-                  </Form.Text>
-                </Modal.Footer>
+              <Modal.Footer>
+                <Form.Text className="text-muted m-3">
+                  すでにアカウントをお持ちの場合、ログイン
+                </Form.Text>
+              </Modal.Footer>
             </Modal>
           </ButtonToolbar>
           <div className="clearfix"></div>

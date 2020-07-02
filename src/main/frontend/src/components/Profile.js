4edfa6c22022beedc36ba8f26f33a2c9795dd2e9
asasin <michi.hiro624@gmail.com>
2020-07-02 13:35:42 +0900

    profileを少し変更

--- src/main/frontend/src/components/Profile.js
+++ src/main/frontend/src/components/Profile.js
@@ -1,13 +1,63 @@
 import React from "react";
+import "../style/Profile.css";
+import { Col, Image,} from "react-bootstrap";
 
 class Profile extends React.Component {
   render() {
     return (
-      <header>
+      <container>
+        <header>
+          <div className="text-right m-3 p-3">
+            プロフィールの編集
+          </div>
+        </header>
+
+        
         <div>
-          プロフィールの編集
+          アイコン
         </div>
-      </header>
+          <Col md={4}>
+            <Image src="" rounded />
+          </Col>
+        
+
+        <div>
+          プロフィール
+        </div>
+        <div>
+          プロフィール文
+        </div>
+
+        <div>
+          活動
+        </div>
+        <div>
+          活動内容
+        </div>
+
+        <div>
+          趣味
+        </div>
+        <div>
+          趣味内容
+        </div>
+
+        <div>
+          SNS
+        </div>
+        <div>
+          SNS一覧
+        </div>
+
+        <div>
+          ギャラリー
+        </div>
+        <div>
+          ギャラリー集
+        </div>
+      </container>
+
+
     );
   }
 }

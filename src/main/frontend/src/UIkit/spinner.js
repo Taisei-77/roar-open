31ea429e45f9126479540c413622a8fb1763cd76
king <haruk@DESKTOP-U7EIVLC>
2020-07-10 21:37:23 +0900

    更新ボタン微調整

--- src/main/frontend/src/UIkit/spinner.js
+++ src/main/frontend/src/UIkit/spinner.js
@@ -3,7 +3,10 @@ import React from "react";
 const Spinner = () => {
   return (
     <div>
-      <div className="spinner-border text-light" role="status">
+      <div
+        className="d-flex justify-content-center spinner-border text-light spinner-border-sm"
+        role="status"
+      >
         <span className="sr-only">Loading...</span>
       </div>
     </div>

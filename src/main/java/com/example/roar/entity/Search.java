3f29cca78f3e1b153058219ef4dec321cbe2d301
king <haruk@DESKTOP-U7EIVLC>
2020-07-13 17:50:16 +0900

    カラム変更に伴う修正

--- src/main/java/com/example/roar/entity/Search.java
+++ src/main/java/com/example/roar/entity/Search.java
@@ -7,31 +7,27 @@ import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.persistence.Id;
 
-//import lombok.AllArgsConstructor;
 import lombok.Data;
-//import lombok.NoArgsConstructor;
 
 @Entity // JPAのエンティティであることを示す。DBで使用するオブジェクトをエンティティ。フィールド=カラム。
 @Data
-// @NoArgsConstructor //引数を持たないコンストラクタを生成
-// @AllArgsConstructor //全ての引数を持つコンストラクタを生成
 @Table(name = "search_info")
 public class Search {
     @Id // 主キーを示す
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(nullable = false)
-    private String teamName; // チーム名
+    private String team_name; // チーム名
     @Column(nullable = true)
     private String picture; // チーム写真
     @Column(nullable = true)
-    private String sportName; // 競技名
+    private String sport_name; // 競技名
     @Column(nullable = true)
     private String prefectures; // 活動地域
     @Column(nullable = true)
-    private String activityFrequency; // 活動頻度
+    private String activity_frequency; // 活動頻度
     @Column(nullable = true)
-    private String dayOfTheWeek; // 活動曜日
+    private String day_of_the_week; // 活動曜日
     @Column(nullable = true)
-    private String teamConcept; // チームコンセプト
+    private String team_concept; // チームコンセプト
 }

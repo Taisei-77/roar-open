2bffe42dacc290de7c773265e5969a062e5d29fa
村上　 泰誠 <korokoro519@gmail.com>
2020-07-15 11:07:04 +0900

    チーム探す画面にページネーション機能を追加

--- src/main/frontend/src/components/TeamSearch.js
+++ src/main/frontend/src/components/TeamSearch.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 import Select from "react-select";
 
@@ -21,19 +21,72 @@ import { TeamSearchCard } from "./TeamSearchCard";
 // データベースとの通信
 const url = "http://localhost:8080/api/search";
 
+// searchResultDataを入れておくための変数
+let searchResultDataBox = "";
+
+// useReducerと連動。
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "reducerUpdate":
+      return {
+        ...state,
+        currentPage: 1,
+        sum: searchResultDataBox.length, //データ総件数
+        per: 10, //1ページあたりの表示件数
+        pageAmount: Math.ceil(searchResultDataBox.length / 10), //ページの総数
+        resourceData: searchResultDataBox.slice(0, 10), //ページに表示するデータ（配列型式）
+      };
+    // 前へページを押した時に実行される
+    case "viewPreview":
+      return {
+        ...state,
+        currentPage: state.currentPage - 1,
+        resourceData: action.data, //表示するデータを書く
+      };
+    case "viewNext":
+      return {
+        ...state,
+        currentPage: state.currentPage + 1,
+        resourceData: action.data, //表示するデータを書く
+      };
+    default:
+  }
+};
+
 const TeamSearch = () => {
+  // ユーザーが検索のために選択した値を管理するstate
   const [sportName, setSportName] = useState(""),
     [prefectures, setPrefectures] = useState(""),
     [activityFrequency, setActivityFrequency] = useState(""),
     [dayOfTheWeek, setDayOfTheWeek] = useState(""),
     [searchResultData, setSearchResultData] = useState([]);
 
+  const [state, dispatch] = useReducer(reducer, {
+    currentPage: 1, //現在のページ
+    sum: searchResultData.length, //データ総件数
+    per: 10, //1ページあたりの表示件数
+    pageAmount: Math.ceil(searchResultData.length / 10), //ページの総数
+    resourceData: searchResultData, //ページに表示するデータ（配列型式）
+  });
+
+  useEffect(() => {
+    searchResultDataBox = searchResultData;
+    return dispatch({ type: "reducerUpdate" });
+  }, [searchResultData]);
+
+  // 検索ボタンを押した時に実行される処理
   const TeamSearchBtn = () => {
     setSportName(document.sport_name.select.value);
     setPrefectures(document.prefectures_name.select.value);
     setActivityFrequency(document.activity_frequency_name.select.value);
     setDayOfTheWeek(document.day_of_the_week_name.select.value);
     SearchInfo();
+    window.scrollTo(0, 0);
+  };
+
+  // 検索結果を表示するデータを切り出してくれる処理。
+  const sliceData = (offset, per) => {
+    return searchResultData.slice(offset, offset + per);
   };
 
   // 検索機能の記述
@@ -53,24 +106,9 @@ const TeamSearch = () => {
       )
       //get(エンドポイント, { params: {送りたいパラメーターの指定}　}
       .then((res) => {
-        setSearchResultData(res.data);
         // 通信に成功後レスポンスが返ってきた時に実行したい処理
         //取得データ全てをリスト化表示
-
-        // for (var i = 0; i < res.data.length; i++) {
-        //   arr = arr.concat(res.data[i]);
-        //   setSearchResultData(JSON.stringify(arr));
-        // }
-
-        //   for (var i = 0; i < res.data.length; i++) {
-        //     arr.push(
-        //       <li key={res.data[i].id}>
-        //         {res.data[i].id}/{res.data[i].user}/{res.data[i].address}/
-        //         {res.data[i].password}
-        //       </li>
-        //     );
-        //   }
-        //   fetchUserInfo(arr);
+        setSearchResultData(res.data);
       })
       .catch((error) => {
         // 通信に失敗してレスポンスが返ってこなかった時に実行したい処理
@@ -82,6 +120,27 @@ const TeamSearch = () => {
   const freeWordChange = (e) => {
     setFreeWord(e.target.value);
   };
+
+  // 前へボタンを押すと実行される処理
+  const viewPreview = () => {
+    window.scrollTo(0, 0);
+    const offset = (state.currentPage - 2) * state.per;
+    return dispatch({
+      type: "viewPreview",
+      data: sliceData(offset, state.per),
+    });
+  };
+
+  // 次へボタンを押すと実行される処理
+  const viewNext = () => {
+    window.scrollTo(0, 0);
+    const offset = state.currentPage * state.per;
+    return dispatch({
+      type: "viewNext",
+      data: sliceData(offset, state.per),
+    });
+  };
+
   return (
     <div>
       <AppBar color="default" position="sticky">
@@ -136,19 +195,41 @@ const TeamSearch = () => {
           </Button>
         </Toolbar>
       </AppBar>
-      <div>{searchResults}</div>
-      {searchResultData.map((data) => (
-        <TeamSearchCard
-          id={data.id}
-          team_name={data.team_name}
-          picture={data.picture}
-          sport_name={data.sport_name}
-          prefectures={data.prefectures}
-          activity_frequency={data.activity_frequency}
-          day_of_the_week={data.day_of_the_week}
-          team_concept={data.team_concept}
-        />
-      ))}
+      <div>
+        {/*　DBから取得した値を1つずつ取り出して、map以下の処理を個数分行う。 */}
+        {state.resourceData.length > 0 ? (
+          state.resourceData.map((data) => (
+            <TeamSearchCard
+              id={data.id}
+              team_name={data.team_name}
+              picture={data.picture}
+              sport_name={data.sport_name}
+              prefectures={data.prefectures}
+              activity_frequency={data.activity_frequency}
+              day_of_the_week={data.day_of_the_week}
+              team_concept={data.team_concept}
+            />
+          ))
+        ) : (
+          <p>検索結果は0件でした。</p>
+        )}
+      </div>
+      <div>
+        <Button onClick={viewPreview} disabled={state.currentPage === 1}>
+          前へ
+        </Button>
+        <span>
+          {state.currentPage}/{state.pageAmount}
+        </span>
+        <Button
+          onClick={viewNext}
+          disabled={
+            state.currentPage === state.pageAmount || state.pageAmount === 0
+          }
+        >
+          次へ
+        </Button>
+      </div>
     </div>
   );
 };

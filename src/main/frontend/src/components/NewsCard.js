e00c123c6580198ab96b1ec45e13ad17e463d076
asasin <michi.hiro624@gmail.com>
2020-07-21 17:30:58 +0900

    不要なコードの削除

--- src/main/frontend/src/components/NewsCard.js
+++ src/main/frontend/src/components/NewsCard.js
@@ -12,71 +12,70 @@ import Typography from "@material-ui/core/Typography";
 export default class NewsCard extends React.Component {
 
     state = {
-        articles: [],//オブジェクトの入れ物
-        refreshing: false,// これは何のためにあるのかは不明
+        articles: []//オブジェクトの入れ物
     }
 
     getNews = async () => {//APIからdataを取り出す処理
         this.setState({ refreshing: true });
         const url = "http://newsapi.org/v2/top-headlines?country=jp&category=sports&apiKey=c68b6097d82e46b7bb27b7d19e3110cf";
-        try {
+        try {//成功させたい処理
             const result = await fetch(url);
             const json = await result.json();
-            //console.log(json);
             this.setState({
-                articles: json.articles,
-                refreshing: false
+                articles: json.articles
             });
-        } catch (e) {
-            this.setState({ refreshing: false });
+        } 
+        catch (e) {//tryに例外が発生するとすぐこちらが呼び出される
             console.log(e);
         }
     }
 
-    componentDidMount = () => { //マウント時に行われる処理。
+    componentDidMount = () => {//マウント時に行われる処理。
         this.getNews();
     }
 
     render() {
-        //console.log(this.state.articles);
         return (//カードの見た目
-            <ul className="d-flex row">
+            <ul>
                 {this.state.articles.map(item => (
                     <li key={item.id} className="m-3 d-block col-md-12">
                     <Card>
                         <Grid container>
                             <Grid item xs={4}>
+                                {/* ニュースのサムネイル */}
                                 <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    alt="ニュース画像"
-                                    height="100%"
-                                    image={item.urlToImage}
-                                    title="ニュース画像"
-                                />
+                                    <CardMedia
+                                        component="img"
+                                        alt="ニュース画像"
+                                        height="100%"
+                                        image={item.urlToImage}
+                                        title="ニュース画像"
+                                    />
                                 </CardActionArea>
                             </Grid>
                             <Grid item xs={8}>
                                 <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    {item.title}
-                                </Typography>
-                                <Typography
-                                    variant="body2"
-                                    color="textSecondary"
-                                    component="p"
-                                >
-                                    {item.description}
-                                </Typography>
-                                <CardActions>
-                                    <Button
-                                    size="small"
-                                    color="primary"
-                                    href={item.url}
+                                    {/* ニュースタイトル */}
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {item.title}
+                                    </Typography>
+                                    {/* ニュースディスクリプション */}
+                                    <Typography
+                                        variant="caption"
+                                        color="textSecondary"
+                                        component="p"
                                     >
-                                    詳細を確認する
-                                    </Button>
-                                </CardActions>
+                                        {item.description}
+                                    </Typography>
+                                    <CardActions>
+                                        <Button
+                                            size="small"
+                                            color="primary"
+                                            href={item.url}
+                                        >
+                                            詳細を確認する
+                                        </Button>
+                                    </CardActions>
                                 </CardContent>
                             </Grid>
                         </Grid>

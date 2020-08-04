0bc04d7e8064b3af1f1eaadace42b1fbdea1ebb1
村上　 泰誠 <korokoro519@gmail.com>
2020-08-04 16:44:42 +0900

    チャット画面の変更。Firestoreから受け取ったデータの加工、表示をした。

--- src/main/frontend/src/components/Chat.js
+++ src/main/frontend/src/components/Chat.js
@@ -1,67 +1,74 @@
 import React, { useState, useEffect } from "react";
-import { db } from "../firebase/index";
 
+import { auth } from "../firebase/index";
+
+import { db } from "../firebase/index";
 import "react-chat-elements/dist/main.css";
 import styles from "../style/Chat.module.css";
-
-// import MessageList from "./MessageList";
-
+import Message from "./Message";
 import ChatGroupList from "./ChatGroupList";
-import { MessageList, Input } from "react-chat-elements";
 import { Button } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 //現在時刻取得
+//使用時はcustomDateを直接使う。
 const date = new Date();
 const customDate =
   date.getFullYear() +
-  "-" +
+  "年" +
   (date.getMonth() + 1) +
-  "-" +
+  "月" +
   date.getDate() +
-  " " +
+  "日" +
   date.getHours() +
   ":" +
-  date.getMinutes() +
-  ":" +
-  date.getSeconds();
+  date.getMinutes(); //+
+// ":"+
+// date.getSeconds();
 
-// fireStoreのChatコレクションのroomドキュメントを参照する変数
-let docRef = db.collection("Chat").doc("room");
-let messageData = []; //colud fireStoreの内容を代入するための変数
-const addMessageData = () => {
-  docRef
-    .get()
-    .then((snapshot) => (messageData = snapshot.data().message))
-    .catch((err) => alert("エラー"));
+//変数の定義
+let docRef = db.collection("Chat").doc("room"); // Cloud FirestoreのChatコレクションのroomドキュメントを参照する変数;
+let messageData = []; //Colud Firestoreの内容を代入するための変数
+
+//関数の定義
+const messageAreaScroll = () => {
+  const messageArea = document.getElementById("messageArea");
+  messageArea.scrollTop = messageArea.scrollHeight;
 };
 
-// メッセージの書き方
-// const messages = [
-//   {
-//     position: "left", //メッセージの位置
-//     type: "text", //メッセージの型
-//     text: "サンプルチャット",　//メッセージのテキスト
-//     date: new Date(customDate), //メッセージの時間
-//   },
-//   {
-//     position: "right",
-//     type: "text",
-//     text: "サンプルチャット2",
-//     date: new Date("2020-03-12 14:00:00"),
-//   },
-// ];
 const Chat = () => {
-  const [resultMessages, setResultMessages] = useState([]);
+  const [resultMessages, setResultMessages] = useState([]), //メッセージ配列を管理する。
+    [loading, isLoading] = useState(false); //描画を管理する。（初回レンダリングの準備が　完了=>ture 未完了=>false）
 
+  //初回のレンダリングのみ起動。
   useEffect(() => {
     const fn = async () => {
-      await addMessageData();
-      setResultMessages(messageData);
+      await initialMessageData();
     };
     fn();
-
-    console.log(messageData);
   }, []);
+
+  //初回レンダリング時に実行。Cloud Firestoreからメッセージ情報を取得して、ResultMessagesの値を取得した情報に置き換える。
+  const initialMessageData = () => {
+    docRef
+      .get()
+      .then(function (doc) {
+        if (doc.exists) {
+          //ドキュメントが存在した場合の処理
+          messageData = doc.data().message; //Chatコレクション>roomドキュメント>データ>メッセージ配列をmessageDataへ代入。
+          console.log("Document data:", doc.data());
+          setResultMessages(messageData); //ResultMessagesの値を書き換える。
+          console.log("a", messageData.message);
+          isLoading(true); //初回レンダリング処理の準備が完了したことを示す。
+        } else {
+          //ドキュメントが存在しなかった場合の処理
+          console.log("No such document!");
+        }
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
+  };
 
   //送信・追加
   const addMessage = () => {
@@ -70,62 +77,86 @@ const Chat = () => {
     // ユーザーが送信する新しいメッセージ情報
     const newMessage = [
       {
-        position: "right", //メッセージを右に配置
+        avatar:
+          "https://firebasestorage.googleapis.com/v0/b/roar-b54b1.appspot.com/o/images%2FIpUsst5ZpOB2GUy5?alt=media&token=82158741-deea-4fd4-bfe2-09ee67c2c6a1",
+        uid: auth.currentUser.uid, //メッセージを右に配置
+        // position: uid === userUid ならright　 uid !== userUid　ならleft
         type: "text",
-        text: messageValue,
-        date: new Date(customDate),
+        text: messageValue, //ユーザーが入力したテキスト
+        dateString: customDate, // 2020/8/20/13:34のように表示される。
       },
     ];
     // 新しいメッセージをCloud Firestoreのメッセージ配列に追加する
-
-    const saveMassage = messageData.concat(newMessage);
-    docRef.set({ message: saveMassage });
+    console.log("b", messageData);
+    messageData = messageData.concat(newMessage);
+    docRef.set({ message: messageData });
     console.log(messageData);
-    setResultMessages(saveMassage);
-    addMessageData();
-    // messageData変数に最新のメッセージデータを再代入する（更新する）。
-    // docRef.get().then((doc) => (messageData = doc.data().message));
-    // メッセージを取得
-    // console.log(messageData);
-    // setResultMessages(messageData);
-    // console.log(resultMessages);
+    console.log(messageData.message);
+    setResultMessages(messageData);
+
+    //textareaの値をクリアする
+    document.getElementById("messageValue").value = "";
+    messageAreaScroll();
   };
-  return (
-    <div>
-      <div className={styles.chat}>
-        <div className={styles.list}>
-          <ChatGroupList />
-        </div>
-        <div className={styles.message}>
-          {/* {messageData.map((data) => (
-            <MessageList dataSource={data} />
-          ))} */}
-          <MessageList
-            toBottomHeight={"100%"}
-            // lockable={true}
-            dataSource={resultMessages}
-          />
-          <div>
-            <input id="messageValue" placeholder="入力" />
-            <Button variant="contained" onClick={() => addMessage()}>
-              送信
-            </Button>
+
+  if (loading === false) {
+    //読み込み中
+    return (
+      <>
+        <p>Loading..................</p>
+      </>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <div className={styles.navBar}>チャット</div>
+        <div className={styles.mainContainer}>
+          <div className={styles.listContainer}>
+            <div className={styles.searchBar}>
+              <input className={styles.listSearchWord} type="text" />
+              <Button>
+                <SearchIcon className={styles.searchIcon} />
+              </Button>
+            </div>
+            <ChatGroupList />
+          </div>
+          <div className={styles.messageContainer}>
+            <div className={styles.searchBar}>
+              <div className={styles.roomName}>チャット相手の名前</div>
+              <input className={styles.messageSearchWord} type="text" />
+              <Button>
+                <SearchIcon className={styles.searchIcon} />
+              </Button>
+            </div>
+            <div id="messageArea" className={styles.messageArea}>
+              {resultMessages.map((data) => (
+                <Message
+                  avatar={data.avatar}
+                  uid={data.uid} //メッセージの位置
+                  text={data.text} //メッセージのテキスト
+                  dateString={data.dateString} //時間
+                  // notch={false} //ノッチ削除
+                  // toBottomHeight={"100%"} //?
+                />
+              ))}
+            </div>
+            <div className={styles.messageActionArea}>
+              <textarea
+                id="messageValue"
+                className={styles.submitText}
+                placeholder="テキストを入力"
+                maxLength="200"
+                minLength="1"
+              />
+              <Button variant="contained" onClick={() => addMessage()}>
+                送信
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Chat;
-
-{
-  /* <ChatItem
-          avatar={"https://facebook.github.io/react/img/logo.svg"}
-          alt={"アイコン"}
-          title={"ユーザー名"}
-          // subtitle={"What are you doing?"}
-          // date={new Date()}
-          unread={1}
-        /> */
-}

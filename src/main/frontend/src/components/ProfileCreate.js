56da19e892e13be7d7c26b1f07c0e11e51f37f25
村上　 泰誠 <korokoro519@gmail.com>
2020-08-08 16:41:44 +0900

    ホーム画面のプロフィール編集をモーダルに、変更前情報はAPIによる取得にした。

--- src/main/frontend/src/components/ProfileCreate.js
+++ src/main/frontend/src/components/ProfileCreate.js
@@ -1,29 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Spinner from "../UIkit/Spinner";
+
 import styles from "../style/ProfileCreate.module.css";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Button, Icon } from "@material-ui/core";
 import { CreatePhoto } from "../UIkit/index";
 import { auth } from "../firebase/index";
 import axios from "axios";
 
 const url = "http://localhost:8080/api/profile";
 
-const ProfileCreate = (props) => {
-  const {
-    beforeIcon,
-    beforeProfile,
-    beforeActivity,
-    beforeLike,
-    beforeSns,
-    beforeGallery,
-  } = props.location.state;
-  const [icon, setIcon] = useState(beforeIcon),
-    [profile, setProfile] = useState(beforeProfile),
-    [activity, setActivity] = useState(beforeActivity),
-    [like, setLike] = useState(beforeLike),
-    [sns, setSns] = useState(beforeSns),
-    [gallery, setGallery] = useState(beforeGallery);
+const ProfileCreate = () => {
+  const [icon, setIcon] = useState(""),
+    [profile, setProfile] = useState(""),
+    [activity, setActivity] = useState(""),
+    [like, setLike] = useState(""),
+    [sns, setSns] = useState(""),
+    [gallery, setGallery] = useState(""),
+    [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     //name属性に応じてvalueをセット
@@ -46,6 +41,28 @@ const ProfileCreate = (props) => {
       //no default
     }
   };
+  //ユーザーのプロフィール情報を取得する
+  const UserInfo = () => {
+    //送信
+    axios
+      .get(url + "/" + auth.currentUser.uid) //パスパラメータにユーザーIDを追加
+      .then((res) => {
+        setIcon(res.data.icon);
+        setProfile(res.data.profile);
+        setActivity(res.data.activity);
+        setLike(res.data.likes);
+        setSns(res.data.sns);
+        setGallery(res.data.gallery);
+        setLoading(!loading);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+  //初回レンダリング時に、ユーザーのプロフィール情報が取得できる。
+  useEffect(() => {
+    UserInfo();
+  }, []);
 
   const handleFormSubmit = (e) => {
     //通常の送信処理等を停止
@@ -62,7 +79,8 @@ const ProfileCreate = (props) => {
         gallery: gallery,
       })
       .then(() => {
-        props.history.push("/Profile");
+        // props.history.push("/Profile");
+        // props.profileEdited;
       })
       .catch((error) => {
         alert(error);
@@ -72,92 +90,95 @@ const ProfileCreate = (props) => {
   const getImages = (url) => {
     setIcon(url);
   };
-
-  return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <div className={styles.profileCreateTitle}>プロフィール編集</div>
-        <div>
+  if (loading === false) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <div className={styles.profileCreateTitle}>プロフィール編集</div>
+          {/* <div>
           <Link className={styles.profileCreatePreviewBtn} to="/Home">
             変更せずに戻る
           </Link>
-        </div>
-      </header>
-      <div className={styles.main}>
-        <Form onSubmit={handleFormSubmit}>
-          {/* <div className={styles.profileItem}>アイコン</div> */}
-          <CreatePhoto
-            height={140}
-            width={140}
-            getImages={getImages}
-            firstImages={beforeIcon}
-            myTitle="アイコン"
-            storageFolder={"images"}
-          />
-          <div className={styles.mainContent}>
-            <div className={styles.mainContentLeft}>
-              <div className={styles.profileItem}>プロフィール</div>
-              <TextField
-                name="profile"
-                multiline
-                rows={4}
-                variant="outlined"
-                value={profile}
-                className={styles.profileItemValue}
-                onChange={handleChange}
-              />
+        </div> */}
+        </header>
+        <div className={styles.main}>
+          <Form onSubmit={handleFormSubmit}>
+            {/* <div className={styles.profileItem}>アイコン</div> */}
+            <CreatePhoto
+              height={140}
+              width={140}
+              getImages={getImages}
+              firstImages={icon}
+              myTitle="アイコン"
+              storageFolder={"images"}
+            />
+            <div className={styles.mainContent}>
+              <div className={styles.mainContentLeft}>
+                <div className={styles.profileItem}>プロフィール</div>
+                <TextField
+                  name="profile"
+                  multiline
+                  rows={4}
+                  variant="outlined"
+                  value={profile}
+                  className={styles.profileItemValue}
+                  onChange={handleChange}
+                />
 
-              <div className={styles.profileItem}>活動</div>
-              <TextField
-                name="activity"
-                multiline
-                rows={4}
-                variant="outlined"
-                value={activity}
-                className={styles.profileItemValue}
-                onChange={handleChange}
-              />
+                <div className={styles.profileItem}>活動</div>
+                <TextField
+                  name="activity"
+                  multiline
+                  rows={4}
+                  variant="outlined"
+                  value={activity}
+                  className={styles.profileItemValue}
+                  onChange={handleChange}
+                />
 
-              <div className={styles.profileItem}>趣味</div>
-              <TextField
-                name="like"
-                multiline
-                rows={4}
-                variant="outlined"
-                value={like}
-                className={styles.profileItemValue}
-                onChange={handleChange}
-              />
+                <div className={styles.profileItem}>趣味</div>
+                <TextField
+                  name="like"
+                  multiline
+                  rows={4}
+                  variant="outlined"
+                  value={like}
+                  className={styles.profileItemValue}
+                  onChange={handleChange}
+                />
 
-              <div className={styles.profileItem}>SNS</div>
-              <TextField
-                name="sns"
-                multiline
-                rows={4}
-                variant="outlined"
-                value={sns}
-                className={styles.profileItemValue}
-                onChange={handleChange}
-              />
-            </div>
-            <div className={styles.mainContentRight}>
+                <div className={styles.profileItem}>SNS</div>
+                <TextField
+                  name="sns"
+                  multiline
+                  rows={4}
+                  variant="outlined"
+                  value={sns}
+                  className={styles.profileItemValue}
+                  onChange={handleChange}
+                />
+              </div>
+              {/* <div className={styles.mainContentRight}>
               <div className={styles.profileItem}>ギャラリー</div>
               <div>
                 <Form.Group>
                   <Form.File name="" value="" />
                 </Form.Group>
               </div>
+            </div> */}
             </div>
-          </div>
-          <div className={styles.submitBtn}>
-            <Button type="submit" variant="contained" color="primary">
-              プロフィールに反映させる
-            </Button>
-          </div>
-        </Form>
+            <div className={styles.submitBtn}>
+              <Button type="submit" variant="contained" color="primary">
+                プロフィールに反映させる
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default ProfileCreate;

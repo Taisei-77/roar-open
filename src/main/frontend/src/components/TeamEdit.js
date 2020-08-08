68b7822f878e084d682a503ae2121352a0b79db6
村上　 泰誠 <korokoro519@gmail.com>
2020-08-08 17:15:14 +0900

    モーダル表示時、スクロール位置がが一番上になる。チーム編集の情報取得方法をAPI取得に変更。

--- src/main/frontend/src/components/TeamEdit.js
+++ src/main/frontend/src/components/TeamEdit.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 // material-UIの読み込み
@@ -12,23 +12,15 @@ import axios from "axios";
 // コンポーネントの読み込み
 import { CreatePhoto } from "../UIkit/index";
 import { TeamProfile } from "./TeamProfile";
+import Spinner from "../UIkit/Spinner";
 // import { CompePartPerform } from "./CompePartPerform";
 
 export const TeamEdit = (props) => {
-  const {
-    team_id,
-    before_team_name,
-    before_picture,
-    before_sport_name,
-    before_prefectures,
-    before_activity_frequency,
-    before_day_of_the_week,
-    before_team_concept,
-  } = props.location.state;
-  const [teamNameValue, setTeamNameValue] = useState(before_team_name), //チーム名
-    [images, setImages] = useState(before_picture), //画像のURL
-    [teamConceptValue, setTeamConceptValue] = useState(before_team_concept); //チームコンセプト
-  //TeamListDetail.jsから受け取ったチーム情報
+  const { team_id } = props.location.state; //TeamListDetailから編集するチームのteam_idを受け取っている。
+  const [teamNameValue, setTeamNameValue] = useState(""), //チーム名
+    [images, setImages] = useState(""), //画像のURL
+    [teamConceptValue, setTeamConceptValue] = useState(""), //チームコンセプト
+    [loading, setLoading] = useState(false);
 
   //　入力値を取得するイベント
   const handleValueChange = (e) => {
@@ -66,6 +58,39 @@ export const TeamEdit = (props) => {
   // データベースのAPI
   const url = "http://localhost:8080/api/search";
 
+  //初回レンダリング時に編集したいチーム情報を取得
+  const getTeamInfo = () => {
+    axios
+      .get(url, {
+        params: {
+          team_name: "",
+          teamId: team_id,
+          sportName: "",
+          prefectures: "",
+          activityFrequency: "",
+          dayOfTheWeek: "",
+          freeWord: "",
+        },
+      })
+      .then((res) => {
+        //res.dataは [{activityFrequency:値 , count:値, dayOfTheWeek:値, picture:値, prefectures:値, sportName:値,
+        //             teamConcept:値, teamId:値,  teamName:値,  }]　この形で返ってくる。
+        const resData = res.data[0];
+        console.log(resData);
+        setTeamNameValue(resData.teamName);
+        setImages(resData.picture);
+        setTeamConceptValue(resData.teamConcept);
+        setLoading(!loading);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+  //初回レンダリング時に実行される。
+  useEffect(() => {
+    getTeamInfo(); //編集したいチーム情報を取得
+  }, []);
+
   //作成ボタン
   const teamCreateBtn = () => {
     //送信
@@ -90,74 +115,70 @@ export const TeamEdit = (props) => {
         alert(error);
       });
   };
-
-  return (
-    <div>
-      <div className={styles.teamEditTitleBar}>
-        <p className={styles.teamEditTitle}>チームの編集</p>
-        <Link className={styles.teamEditPreviewBtn} to="/Home">
-          変更せずに戻る
-        </Link>
-      </div>
-      <div className={styles.teamCreateBody}>
-        <div className={styles.teamCreateContainer}>
-          <TextField
-            id="teamName"
-            label="チーム名"
-            name="teamName"
-            value={teamNameValue}
-            defaultValue={before_team_name}
-            onChange={handleValueChange}
-          />
+  if (loading === false) {
+    return <Spinner />;
+  } else {
+    return (
+      <div>
+        <div className={styles.teamEditTitleBar}>
+          <p className={styles.teamEditTitle}>チームの編集</p>
+          <Link className={styles.teamEditPreviewBtn} to="/Home">
+            変更せずに戻る
+          </Link>
         </div>
-        <CreatePhoto
-          className={styles.teamCreateContainer}
-          height={450}
-          width={700}
-          myTitle="チームの写真"
-          getImages={getImages}
-          firstImages={before_picture}
-          storageFolder={"team_images"}
-        />
-        {/* <div className={styles.basicProfile}>基本プロフィール</div> */}
-        <TeamProfile
-          className={styles.teamCreateContainer}
-          before_sport_name={before_sport_name}
-          before_prefectures={before_prefectures}
-          before_activity_frequency={before_activity_frequency}
-          before_day_of_the_week={before_day_of_the_week}
-        />
-        <div className={styles.teamCreateContainer}>
-          <TextField
-            id="teamConcept"
-            label="チームコンセプト"
-            multiline
-            rows={4}
-            className={styles.teamConcept}
-            // DBに値を送るための記述
-            name="teamConcept"
-            value={teamConceptValue}
-            onChange={handleValueChange}
+        <div className={styles.teamCreateBody}>
+          <div className={styles.teamCreateContainer}>
+            <TextField
+              id="teamName"
+              label="チーム名"
+              name="teamName"
+              value={teamNameValue}
+              onChange={handleValueChange}
+            />
+          </div>
+          <CreatePhoto
+            className={styles.teamCreateContainer}
+            height={450}
+            width={700}
+            myTitle="チームの写真"
+            getImages={getImages}
+            firstImages={images}
+            storageFolder={"team_images"}
           />
-        </div>
-        {/* <div className={styles.teamCreateContainer}>
+          {/* <div className={styles.basicProfile}>基本プロフィール</div> */}
+          <TeamProfile className={styles.teamCreateContainer} />
+          <div className={styles.teamCreateContainer}>
+            <TextField
+              id="teamConcept"
+              label="チームコンセプト"
+              multiline
+              rows={4}
+              className={styles.teamConcept}
+              // DBに値を送るための記述
+              name="teamConcept"
+              value={teamConceptValue}
+              onChange={handleValueChange}
+            />
+          </div>
+          {/* <div className={styles.teamCreateContainer}>
           <span>大会参加実績</span>
           <Button onClick={handleClick}>追加する</Button>
           <div>{compePartPerforms}</div>
         </div> */}
-        <div className={styles.teamCreateButton}>
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            onClick={teamCreateBtn}
-          >
-            上記の内容に変更
-          </Button>
+          <div className={styles.teamCreateButton}>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              onClick={teamCreateBtn}
+            >
+              上記の内容に変更
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default TeamEdit;

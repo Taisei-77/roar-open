4b08b5976f01c4b0716d063808c9bafb5c0a4c00
king <haruk@DESKTOP-U7EIVLC>
2020-08-15 19:57:34 +0900

    チャット未読機能　実装

--- src/main/frontend/src/components/Message.js
+++ src/main/frontend/src/components/Message.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../style/Message.module.css";
-import { auth } from "../firebase/index";
+import { auth, db } from "../firebase/index";
 
 const Message = (props) => {
+  const [name, setName] = useState("");
   const currentUid = auth.currentUser.uid;
+
+  //ユーザー名を表示する処理
+  useEffect(() => {
+    if (props.uid !== currentUid) {
+      //message取得
+      const unsubscribe = db
+        .collection("users")
+        .doc(props.uid)
+        .onSnapshot(
+          function (doc) {
+            setName(doc.data().name);
+          },
+          function (error) {
+            alert("ユーザー名取得エラー：" + error);
+          }
+        );
+      //コンポーネント破棄時onSnapshot解除
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, []);
+
   if (props.uid === currentUid) {
     return (
       //現在ログインしているユーザーが送信したメッセージの描画
@@ -23,6 +47,7 @@ const Message = (props) => {
     return (
       //現在ログインしているユーザー以外が送信したメッセージの描画
       <div className={`${styles.messageLeft} ${styles.MessageContainer}`}>
+        <p className={styles.name}>{name}</p>
         <div className={styles.message}>
           <img className={styles.avatar} src={props.avatar} />
           <div className={`${styles.textLeft} ${styles.messageText}`}>
